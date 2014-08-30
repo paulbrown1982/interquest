1b3f83aab52fd38365aaea66544605dbdbeae073
Charles Dupont <charles@rangespan.com>
2014-08-30 22:28:42 +0100

    latest version

--- scene1/application.js
+++ scene1/application.js
@@ -1,14 +1,21 @@
-
-
 var data={
     background_image: "images/unicorn-background.jpg",
-    orverlay_images:[
+    orverlayImages:[
         {
-            image_url:"images/scroll.jpg",
+            imageUrl:"images/scroll.jpg",
             position: [
                 100,
                 100
-            ]
+            ],
+            description: "scrolls are really cool."
+        },
+        {
+            imageUrl:"images/rocket.jpg",
+            position:[
+                500,
+                50
+            ],
+            description:"rockets are also cool!"
         }
     ]
 }
@@ -18,11 +25,13 @@ var root_component = React.createClass({
     render: function(){
         var props = this.props;
         var background_image = props.background_image;
-        var overlay_images = props.orverlay_images;
+        var overlay_images = props.orverlayImages;
 
         return React.DOM.div({},
             React.DOM.img({src:background_image }, null ),
-            overlay_component(overlay_images[0])
+            overlay_images.map(overlayComponent),
+            textComponent(),
+            inventoryComponent()
         );
     }
 
@@ -30,15 +39,26 @@ var root_component = React.createClass({
 
 
 
-var overlay_component = React.createClass({
+var overlayComponent = React.createClass({
+    
+    addToInventory: function(){
+        
+    },
+    
+    onClick: function(){
+        var props = this.props;
+        var description = props.description;
+        alert(description);
+    },
+    
     render: function(){
         var props = this.props;
-        var image_url = props.image_url;
+        var imageUrl = props.imageUrl;
         var position = props.position;
-        
         var transform = "translate(" + position[0] + "px, " + position[1] + "px)";
         var divStyle = {
-            src:image_url,
+            src:imageUrl,
+            onClick: this.onClick,
             style: {
                 position: "absolute",
                 top: 0,
@@ -51,12 +71,153 @@ var overlay_component = React.createClass({
     }
 });
 
+var textComponent = React.createClass({
+    render: function(){
+        return React.DOM.div(
+            {
+                style:{
+                    backgroundColor:"brown",
+                    display:"inline-block"
+                }
+            },
+            "Text will go here"
+        );
+    }
+})
+
+
+var inventoryComponent = React.createClass ({
+    render: function(){
+        return React.DOM.div(
+        {
+            style:
+            {
+                backgroundColor:"beige" ,
+                display:"inline-block"
+            }
+        },
+        "inventory will go here");
+    }
+    
+});
+
 
 var run_application = function() {
     var main_element_id = document.getElementById('content');
 
     React.renderComponent(root_component(data), main_element_id);
-    // test
+
+}
+
+window.onload=run_application;var data={
+    background_image: "images/unicorn-background.jpg",
+    orverlayImages:[
+        {
+            imageUrl:"images/scroll.jpg",
+            position: [
+                100,
+                100
+            ],
+            description: "scrolls are really cool."
+        },
+        {
+            imageUrl:"images/rocket.jpg",
+            position:[
+                500,
+                50
+            ],
+            description:"rockets are also cool!"
+        }
+    ]
+}
+
+var root_component = React.createClass({
+
+    render: function(){
+        var props = this.props;
+        var background_image = props.background_image;
+        var overlay_images = props.orverlayImages;
+
+        return React.DOM.div({},
+            React.DOM.img({src:background_image }, null ),
+            overlay_images.map(overlayComponent),
+            textComponent(),
+            inventoryComponent()
+        );
+    }
+
+});
+
+
+
+var overlayComponent = React.createClass({
+    
+    addToInventory: function(){
+        
+    },
+    
+    onClick: function(){
+        var props = this.props;
+        var description = props.description;
+        alert(description);
+    },
+    
+    render: function(){
+        var props = this.props;
+        var imageUrl = props.imageUrl;
+        var position = props.position;
+        var transform = "translate(" + position[0] + "px, " + position[1] + "px)";
+        var divStyle = {
+            src:imageUrl,
+            onClick: this.onClick,
+            style: {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                transform: transform
+                }
+        };
+        
+        return React.DOM.img(divStyle, null);
+    }
+});
+
+var textComponent = React.createClass({
+    render: function(){
+        return React.DOM.div(
+            {
+                style:{
+                    backgroundColor:"brown",
+                    display:"inline-block"
+                }
+            },
+            "Text will go here"
+        );
+    }
+})
+
+
+var inventoryComponent = React.createClass ({
+    render: function(){
+        return React.DOM.div(
+        {
+            style:
+            {
+                backgroundColor:"beige" ,
+                display:"inline-block"
+            }
+        },
+        "inventory will go here");
+    }
+    
+});
+
+
+var run_application = function() {
+    var main_element_id = document.getElementById('content');
+
+    React.renderComponent(root_component(data), main_element_id);
+
 }
 
 window.onload=run_application;
