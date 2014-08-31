cefed731a07320daa2cdca40f531d5d0ec1f7585
Bryan Yap <bryan.yap@alliants.com>
2014-08-31 08:52:38 +0100

    add grayscale filtering support for Firefox

--- scene1/application.js
+++ scene1/application.js
@@ -44,6 +44,7 @@
 
         var remaining = [];
         if (props.disabled) {
+          attrs.style.filter = 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale")';
           attrs.style.WebkitFilter = 'grayscale(100%)';
           attrs.style.cursor = 'pointer';
           attrs.onClick = function () {
